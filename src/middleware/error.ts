04436ce84df65928758a5a78b99761b19598d6ea
Diana <diana101210@gmail.com>
2024-11-06 03:47:59 -0500

    created funcionality about authentication y authorization

---
+++ src/middleware/error.ts
@@ -0,0 +1,34 @@
+import { NextFunction, Request, Response } from "express";
+
+export class ApiError extends Error{
+    
+    status: number;
+    details?: Record<string, any>
+
+    constructor(message:string, status:number, details?: Record<string, any>){
+        super(message),
+        this.status = status;
+        this.details = details;
+    }
+}
+
+export default function errorHandler(error:Error, _req:Request, res:Response, _next:NextFunction){
+     if(error instanceof ApiError){
+        res.status(error.status).json({
+            ok:false,
+            error:{
+                message: error.message,
+                details: error.details
+            }
+        });
+     }else{
+        res.status(500).json({
+            ok:false,
+            error:{
+                message: "Error internal server",
+                error: error.stack
+            }
+        })
+
+     }
+}
